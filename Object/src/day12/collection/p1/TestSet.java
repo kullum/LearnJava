92190c331484aaf20f4af90636044e15c1bb0aeb
kullum <kullum@163.com>
2016-07-29 10:37:38 +0800

    Collection Set

---
+++ Object/src/day12/collection/p1/TestSet.java
@@ -0,0 +1,76 @@
+package day12.collection.p1;
+
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
+
+import org.junit.Test;
+
+public class TestSet {
+	@Test
+	public void teatSet1(){
+		//元素的顺序是经过hash校验后得到一个hash值，按照hash值对号入座，
+		//判断一个重复元素时，先判断hash值是否相等，若想等再判断equals方法，
+		//若hash值相等但equals不等（避免此情况出现），仍会被存储
+		HashSet set = new HashSet();
+		set.add(123);
+		set.add(456);
+		set.add("Hello");
+		set.add(new Person("MM",21));
+		set.add(new Person("MM",21));
+		System.out.println(set);
+		//先用hash算法对元素进行重复校验，然后再用双向链表进行存储。
+		LinkedHashSet set1 = new LinkedHashSet();
+		set1.add(123);
+		set1.add(456);
+		set1.add("Hello");
+		set1.add(new Person("MM",21));
+		set1.add(new Person("MM",21));
+		System.out.println(set1);
+		
+		//TreeSet要求所存储的元素均为同一类型的元素
+		//自然排序在自定义类中继承Comparable类并重写compareTo方法
+		TreeSet set2 = new TreeSet();
+		//在自定义类中继承Comparable类并重写compareTo方法
+		set2.add(new Person("MM",21));
+		set2.add(new Person("GG",23));
+		System.out.println(set2);
+		
+		//定制排序  主要用于无法或不便于直接修改自定义类时，因此在外部临时添加。
+		//1. 创建一个实现了Comparator接口的类对象
+		Comparator com = new Comparator(){
+			@Override
+			public int compare(Object o1, Object o2) {
+				if(o1 instanceof Person && o2 instanceof Person){
+					Person p1 = (Person) o1;
+					Person p2 = (Person) o2;
+					return p1.getName().compareTo(p2.getName());
+				}
+				return 0;
+			}	
+		};
+		//2. 将此对象作为形参传递给TreeSet的构造器中
+		TreeSet set3 = new TreeSet(com);
+		set3.add(new Person("MM",21));
+		set3.add(new Person("GG",23));
+		System.out.println(set3);
+		
+		//若该定制排序只用在某个特殊的集合中，则可以将Comparator的匿名对象传入TreeSet的构造器
+		TreeSet set4 = new TreeSet(new Comparator(){
+			@Override
+			public int compare(Object o1, Object o2) {
+				if(o1 instanceof Person && o2 instanceof Person){
+					Person p1 = (Person) o1;
+					Person p2 = (Person) o2;
+					return p1.getName().compareTo(p2.getName());
+				}
+				return 0;
+			}
+		});
+		set4.add(new Person("MM",21));
+		set4.add(new Person("GG",23));
+		System.out.println(set4);
+		
+	}
+}
